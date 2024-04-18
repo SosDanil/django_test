7e4edc29e8ab71f8518c3c3b95ccdd9ff9c99e0e
Danil Klimenko <sos.danil@yandex.ru>
2024-04-18 15:42:54 +0500

    создали новую модель Материал со всеми методами CRUD

--- main/urls.py
+++ main/urls.py
@@ -1,13 +1,15 @@
 from django.urls import path
 
 from main.apps import MainConfig
-from main.views import index, contact, view_student
+from main.views import contact, StudentListView, StudentDetailView
 
 app_name = MainConfig.name
 
 # Пустые кавычки - идем от корня
 urlpatterns = [
-    path('students/', index, name='index'),
+    path('students/', StudentListView.as_view(), name='index'),
+    # path('students/', index, name='index'),
     path('contact/', contact, name='contact'),
-    path('view/<int:pk>/', view_student, name='view_student'),
+    path('view/<int:pk>/', StudentDetailView.as_view(), name='view_student')
+    # path('view/<int:pk>/', view_student, name='view_student'),
 ]
--- main/views.py
+++ main/views.py
@@ -1,16 +1,23 @@
 from django.shortcuts import render, get_object_or_404
+from django.views.generic import ListView, DetailView
 
 from main.models import Student
 
 
-# Create your views here.
-def index(request):
-    students_list = Student.objects.all()
-    context = {
-        'object_list': students_list,
-        'title': 'Главная'
-    }
-    return render(request, 'main/index.html', context)
+class StudentListView(ListView):
+    model = Student
+    template_name = 'main/index.html'
+
+
+# Функцию индекса заменили классовым методом - FBV сменили на CBV
+
+# def index(request):
+#     students_list = Student.objects.all()
+#     context = {
+#         'object_list': students_list,
+#         'title': 'Главная'
+#     }
+#     return render(request, 'main/index.html', context)
 
 
 def contact(request):
@@ -27,9 +34,14 @@ def contact(request):
     return render(request, 'main/contact.html', context)
 
 
-def view_student(request, pk):
-    student_item = get_object_or_404(Student, pk=pk)
-    context = {
-        'object': student_item,
-    }
-    return render(request, 'main/student_detail.html', context)
+class StudentDetailView(DetailView):
+    model = Student
+    template_name = 'main/student_detail.html'
+
+
+# def view_student(request, pk):
+#     student_item = get_object_or_404(Student, pk=pk)
+#     context = {
+#         'object': student_item,
+#     }
+#     return render(request, 'main/student_detail.html', context)
